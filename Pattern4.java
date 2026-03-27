f40c2d844eebbf59d8b571bb79456b9ab437a326
Kummari Prasanna <www.prasannakummari9347@gmail.com>
2026-03-27 21:44:30 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,21 @@
+package JavaPatterns;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=5;j++) {
+				if(i>=j) {
+					System.out.print(i);
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println(" ");
+		}
+
+
+	}
+
+}
