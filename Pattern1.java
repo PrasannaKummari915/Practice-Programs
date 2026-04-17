3c0656e3000e3fe715c3a41e4d959b766f924026
Kummari Prasanna <www.prasannakummari9347@gmail.com>
2026-04-17 19:22:20 +0530

    Add files via upload

--- Pattern1.java
+++ Pattern1.java
@@ -1,21 +1,15 @@
-package JavaPatterns;
+package CoreJavaProgram;
 
 public class Pattern1 {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=5;i++) {
-			for(int j=1;j<=5;j++) {
-				if(i<=j) {
-					System.out.print("*");
-				}
-				else {
-					System.out.print(" ");
-				}
-			}
-			System.out.println(" ");
-		}
-
-
+             int i,j;
+             for(i=0;i<5;i++) {
+            	 for(j=0;j<5;j++) {
+            		 System.out.print("* ");
+            	 }
+            	 System.out.println();
+             }
 	}
 
 }
--- Pattern2.java
+++ Pattern2.java
@@ -1,22 +1,16 @@
-package JavaPatterns;
+package CoreJavaProgram;
 
 public class Pattern2 {
 
-
-		public static void main(String[] args) {
-			for(int i=1;i<=5;i++) {
-				for(int j=1;j<=5;j++) {
-					if(i<=j) {
-						System.out.print(i);
-					}
-					else {
-						System.out.print(" ");
-					}
-				}
-				System.out.println(" ");
-			}
-
-
-		}
+	public static void main(String[] args) {
+		int i,j;
+        for(i=1;i<=5;i++) {
+       	 for(j=1;j<=5;j++) {
+       		 System.out.print(i+" ");
+       	 }
+       	 System.out.println();
+        }
 
 	}
+
+}
